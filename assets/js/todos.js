2e8b6cbdeca4435421cf5bf47594976b20704411
CodingMagesty <codingmagesty@gmail.com>
2018-02-03 20:26:49 +0300

    Slight refactoring for existing functions; Adding todos is avalible;

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,12 +1,22 @@
 // Check off specific Todos when clicking
-$("li").click(function() {
+$("ul").on("click", "li", function() {
   $(this).toggleClass("completed");
 });
 
 // Click on X to delete ToDo
-$("span").click(function(event){
+$("ul").on("click", "span", function(event){
   $(this).parent().fadeOut(500, function() {
     $(this).remove();
   });
   event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event) {
+  if(event.which === 13) {
+    // grabbing new todo text from input
+    var todoText = $(this).val();
+    $(this).val("");
+    // creating new li
+    $("ul").append("<li><span>X</span> " + todoText + "</li>");
+  }
 });
